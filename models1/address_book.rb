db698c80dce0cc34a9c20b42b096bec6e88b6cf0
JJ Cordz <jj@digitalchaos.co>
2017-08-21 20:01:27 -0700

    WIP: Checkpoint 20 Menu

--- models1/address_book.rb
+++ models1/address_book.rb
@@ -1,3 +1,15 @@
+require_relative 'controllers/menu_controller'
+
+# #4
+menu = MenuController.new
+# #5
+system "clear"
+puts "Welcome to AddressBloc!"
+# #6
+menu.main_menu
+
+
+
  require_relative 'entry'
 
    class AddressBook
